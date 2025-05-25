a9f50cd8334f1f9d4cd3b88edef6c0d9a129f051
Dzanija Ivan <ivan.dzanija@tutamail.com>
2025-05-25 17:33:09 +0200

    auto-commit

---
+++ lab03/lab3py/parser.py
@@ -0,0 +1,28 @@
+class Parser:
+    def __init__(self):
+        self.path = None
+
+    def setPath(self, path):
+        self.path = path
+
+    def parseToDataframe(self, dataframe):
+        if self.path is None:
+            print("Path not set. Use setPath()!")
+            return None
+
+        if not isinstance(dataframe, list):
+            print("Dataframe must be a list of dictionaries!")
+            return None
+
+        with open(self.path, "r") as file:
+            lines = file.readlines()
+        features = lines[0].strip().split(",")
+        target = features[-1]
+
+        for index, line in enumerate(lines[1:]):
+            values = line.strip().split(",")
+            dataframe.append(dict())
+            for i, feature in enumerate(features):
+                dataframe[index][feature] = values[i]
+
+        return target
--- lab03/lab3py/solution.py
+++ lab03/lab3py/solution.py
@@ -1,190 +1,65 @@
 from math import log2
+from collections import Counter, defaultdict
+from parser import Parser
 import sys
 
 
-def entropy(data):
-    totalCount = 0
-    for value in data.values():
-        totalCount += value
-
-    if totalCount == 0:
+def entropy(data, target):
+    if not data:
         return 0
 
-    entropy = 0.0
-    for value, count in data.items():
-        probability = count / totalCount
-        entropy -= probability * log2(probability)
-
-    return entropy
+    total = len(data)
+    labels = [row[target] for row in data]
+    counts = Counter(labels)
+    return -sum((count / total) * log2(count / total) for count in counts.values())
 
 
 def informationGain(data, feature, target):
-    totalCount = len(data[target])
-    if totalCount == 0:
-        return 0
+    totalEntropy = entropy(data, target)
+    features = defaultdict(list)
 
-    featureValues = data[feature]
-    featureCounts = dict()
-    for value in featureValues:
-        if value not in featureCounts:
-            featureCounts[value] = 0
-        featureCounts[value] += 1
+    for row in data:
+        features[row[feature]].append(row)
 
-    totalEntropy = entropy(data[target])
-    weightedEntropy = 0.0
+    return totalEntropy - sum(
+        len(feature) / len(data) * entropy(feature, target)
+        for feature in features.values()
+    )
 
-    for value, count in featureCounts.items():
-        subset = {k: v for k, v in data.items() if v[feature] == value}
-        subsetEntropy = entropy(subset[target])
-        weightedEntropy += (count / totalCount) * subsetEntropy
 
-    return totalEntropy - weightedEntropy
+def mostCommon(data, target):
+    if not data:
+        return None
+
+    labels = [row[target] for row in data]
+    return Counter(labels).most_common(1)[0][0]
 
 
 class Node:
-    def __init__(self, feature=None, isLeaf=False):
-        self.isLeaf = isLeaf
+    def __init__(self, isLeaf, label=None, feature=None, children=None):
         self.feature = feature
-
+        self.children = children
+        self.isLeaf = isLeaf
+        self.label = label
 
 
 class ID3:
     def __init__(self, target, features, depth=float("inf")):
         self.target = target
-        self.features = features
+        self.allFeatures = features
         self.maxDepth = depth
         self.resultingTree = None
 
-        self.entropyCache = dict()
-        self.featureSplit = dict()
-        self.featuresCategories = dict()
-        self.expectedEntropy = dict()
-
-        self.target = None
-        self.features = None
-
-    def categorizeFeatures(self, data):
-        if self.features is None:
-            self.features = list(data.keys())
-
-        for feature in self.features:
-            if feature not in self.featuresCategories:
-                self.featuresCategories[feature] = dict()
-            if feature not in self.entropyCache:
-                self.entropyCache[feature] = dict()
-
-            for value in data[feature]:
-                if value not in self.featuresCategories[feature]:
-                    self.featuresCategories[feature][value] = dict()
-                if value not in self.entropyCache[feature]:
-                    self.entropyCache[feature][value] = 0
-
-            for row, value in enumerate(data[self.target]):
-                if value not in self.featuresCategories[feature][data[feature][row]]:
-                    self.featuresCategories[feature][data[feature][row]][value] = 0
-                self.featuresCategories[feature][data[feature][row]][value] += 1
-
-            for value in data[feature]:
-                self.entropyCache[feature][value] = entropy(
-                    self.featuresCategories[feature][value]
-                )
-
-        # print(self.entropyCache)
-
+    def fit(self, data):
+        self.resultingTree = self.buildTree(data, data, set(self.allFeatures), 0)
         return 0
 
-    def entropyFeatures(self, data):
-        totalCount = len(data[self.target])
-        if totalCount == 0:
-            return 0
-
-        if self.features is None:
-            # Should not happen, but just for linting
-            self.features = list(data.keys())
-
-        for feature in self.features:
-            if feature not in self.featureSplit:
-                self.featureSplit[feature] = {}
-            if feature not in self.expectedEntropy:
-                self.expectedEntropy[feature] = 0
-
-            currentValues = data[feature]
-            for value in currentValues:
-                print(f"Processing feature: {feature}, value: {value}")
-                if value not in self.featureSplit[feature]:
-                    self.featureSplit[feature][value] = 0
-                self.featureSplit[feature][value] += 1
-
-            self.expectedEntropy[feature] = sum(
-                self.entropyCache[feature][value] * count / totalCount
-                for value, count in self.featureSplit[feature].items()
-            )
-
-        print(self.expectedEntropy)
-        return 0
-
-    def mostCommonValue(self, data, feature):
-        if feature not in self.featuresCategories:
-            return None
-
-        maxCount = -1
-        mostCommon = str()
-        for value, count in self.featuresCategories[feature].items():
-            if count > maxCount:
-                maxCount = count
-                mostCommon = value
-            if count == maxCount:
-                mostCommon = max(mostCommon,value)
-
-        return mostCommon
-
-    def fit(self, data, parent, features=None, link=None, depth=0):
-        if depth == 0:
-            if self.categorizeFeatures(data):
-                print("Error categorizing features!")
-            if self.entropyFeatures(data):
-                print("Error calculating entropy!")
-
-        if len(data) == 0:
-
-
-
-        return 0
+    def buildTree(self, data, parent_data, features, depth):
+        if not data:
+            return Node(isLeaf=True, label=mostCommon(parent_data, self.target))
 
     def predict(self, data):
         return 0
-
-
-class Parser:
-    def __init__(self):
-        self.path = None
-
-    def setPath(self, path):
-        self.path = path
-
-    def parseToDataframe(self, dataframe):
-        if self.path is None:
-            print("Path not set. Use setPath()!")
-            return None
-
-        if not isinstance(dataframe, dict):
-            print("Dataframe must be a dictionary!")
-            return None
-
-        with open(self.path, "r") as file:
-            lines = file.readlines()
-        features = lines[0].strip().split(",")
-        target = features[-1]
-
-        for feature in features:
-            dataframe[feature] = []
-
-        for line in lines[1:]:
-            values = line.strip().split(",")
-            for i, feature in enumerate(features):
-                dataframe[feature].append(values[i])
-
-        return target
 
 
 def main(args):
@@ -205,7 +80,7 @@ def main(args):
 
     # Parse the training set
     parser.setPath(training_set_path)
-    trainingDF = dict()
+    trainingDF = list()
     target = parser.parseToDataframe(trainingDF)
     if trainingDF is None:
         print("Error parsing training set.")
@@ -213,15 +88,17 @@ def main(args):
 
     # Parse the test set
     parser.setPath(test_set_path)
-    testDF = dict()
+    testDF = list()
     test_target = parser.parseToDataframe(testDF)
     if testDF is None:
         print("Error parsing test set.")
         return 1
 
     # Initialize the ID3 algorithm
-    id3 = ID3(depth=tree_depth, target=target, features=list(trainingDF.keys()))
-    id3.fit(trainingDF, target)
+    modelID3 = ID3(
+        target=target, features=set(trainingDF[0].keys()) - {target}, depth=tree_depth
+    )
+    modelID3.fit(trainingDF)
 
     return 0
 
